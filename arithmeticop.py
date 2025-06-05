b39c5540b49964ffd6ac99730ba15c117c0eb2cd
fathimadiyana <diyanaernad@gmail.com>
2025-06-05 22:55:50 +0530

    Add files via upload

---
+++ arithmeticop.py
@@ -0,0 +1,10 @@
+num1=int(input("enter a num"))
+num2=int(input("enter a num2"))
+
+print("add",num1+num2)
+print("sub",num1-num2)
+print("mult",num1*num2)
+print("div",num1/num2)
+print("mod",num1%num2)
+print("floor div",num1//num2)
+print("exp",num1**num2)
