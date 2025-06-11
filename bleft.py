b7e4c4f7bba20e3b292964b93da49e4aee28bd5a
fathimadiyana <diyanaernad@gmail.com>
2025-06-11 18:41:26 +0530

    Add files via upload

---
+++ bleft.py
@@ -0,0 +1,9 @@
+
+
+num = 5        
+shifted = num << 1  
+
+print("Original number:", num)
+print("Binary:", bin(num))
+print("After left shift by 1:", shifted)
+print("Binary after shift:", bin(shifted))
---
+++ bright.py
@@ -0,0 +1,6 @@
+num = 20       
+shifted = num >> 2
+print("Original number:", num)
+print("Binary:", bin(num))
+print("After right shift by 2:", shifted)
+print("Binary after shift:", bin(shifted))
