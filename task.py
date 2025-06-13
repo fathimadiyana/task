cb4eb7af7bc6decc75205c5981fdb0f51ce1b590
fathimadiyana <diyanaernad@gmail.com>
2025-06-13 19:23:16 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,15 @@
+
+fruits = ["apple", "banana"]
+print(fruits)
+
+
+point = (5, 10)
+print(point)
+
+
+student = {"name": "Tom", "age": 18}
+print(student)
+
+
+numbers = {1, 2, 2, 3}
+print(numbers)
